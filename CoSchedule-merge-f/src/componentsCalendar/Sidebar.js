1dc8d893fd284e76abffca3ec814391bf931020d
Md Talib Ansari <95065465+talib789@users.noreply.github.com>
2022-11-01 14:22:38 +0530

    completed

--- CoSchedule-merge-f/src/componentsCalendar/Sidebar.js
+++ CoSchedule-merge-f/src/componentsCalendar/Sidebar.js
@@ -3,7 +3,7 @@ import CreateEventButton from "./CreateEventButton";
 import SmallCalendar from "./SmallCalendar";
 import Labels from "./Labels";
 export default function Sidebar() {
-  return (
+   return (
     <aside className="border p-5 w-64">
       <CreateEventButton />
       <SmallCalendar />
