b57b0cb8fa69a3aa8b5ca4f2ae2f1f86e46db958
Md Talib Ansari <95065465+talib789@users.noreply.github.com>
2022-11-01 14:33:20 +0530

    completed

--- CoSchedule-merge-f/src/App.js
+++ CoSchedule-merge-f/src/App.js
@@ -2,22 +2,15 @@ import AllRoutes from './component/AllRoutes';
 
 import { Routes,Route } from "react-router-dom";
 import { Homepage } from "./components/home";
-
 import Calendar from './components1/Calendar';
 import React from "react";
 
 function App() {
-
-
   return (
     <div>
-
       <AllRoutes/>
-
-
       </div>
  
   );
 }
-
 export default App;
