e84e49ebd34d3ddb14ce15a123983757e59cf3e0
Md Talib Ansari <95065465+talib789@users.noreply.github.com>
2022-11-01 14:41:57 +0530

    completed

--- coSchedule-backend/Database/index.js
+++ coSchedule-backend/Database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-async function connectDatabase(){
+ async function connectDatabase(){
     try {
         await mongoose.connect("mongodb://127.0.0.1:27017/task")
         console.log("connect");
