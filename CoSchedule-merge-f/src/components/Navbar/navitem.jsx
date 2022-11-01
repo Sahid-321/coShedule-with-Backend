3e04d0044fa02a8e1666e924a086f0832522e342
Md Talib Ansari <95065465+talib789@users.noreply.github.com>
2022-11-01 14:17:44 +0530

    completed

--- CoSchedule-merge-f/src/components/Navbar/navitem.jsx
+++ CoSchedule-merge-f/src/components/Navbar/navitem.jsx
@@ -17,7 +17,7 @@ export default function NavItem({ icon, title, description, active, navSize }) {
             flexDir="column"
             w="100%"
             alignItems={navSize === "small" ? "center" : "flex-start"}
-        >
+           >
             <Menu placement="right">
                 <Link
                     backgroundColor={active && "#d17760"}
@@ -26,7 +26,7 @@ export default function NavItem({ icon, title, description, active, navSize }) {
                     bg= "#d17760"
                     _hover={{ textDecor: 'none', backgroundColor: "#d17760" }}
                     w={navSize === "large" && "100%"}
-                >
+                  >
                     <MenuButton w="100%">
                         <Flex>
                             <Icon as={icon} fontSize="xl" color={active ? "white" : "gray.500"} />
@@ -40,7 +40,7 @@ export default function NavItem({ icon, title, description, active, navSize }) {
                     w={200}
                     h={200}
                     ml={5}
-                >
+                 >
                     <NavHoverBox title={title}  description={description} />
                 </MenuList>
             </Menu>
