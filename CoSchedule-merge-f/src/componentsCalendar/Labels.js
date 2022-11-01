d5f0a39257cf17db201e288b68ea3fe652249d6b
Md Talib Ansari <95065465+talib789@users.noreply.github.com>
2022-11-01 14:23:23 +0530

    completed

--- CoSchedule-merge-f/src/componentsCalendar/Labels.js
+++ CoSchedule-merge-f/src/componentsCalendar/Labels.js
@@ -4,7 +4,7 @@ import GlobalContext from "../contextCalendar/GlobalContext";
 export default function Labels() {
   const { labels, updateLabel } = useContext(GlobalContext);
   return (
-    <React.Fragment>
+     <React.Fragment>
       <p className="text-gray-500 font-bold mt-10">Label</p>
       {labels.map(({ label: lbl, checked }, idx) => (
         <label key={idx} className="items-center mt-3 block">
@@ -13,7 +13,7 @@ export default function Labels() {
             checked={checked}
             onChange={() =>
               updateLabel({ label: lbl, checked: !checked })
-            }
+             }
             className={`form-checkbox h-5 w-5 text-${lbl}-400 rounded focus:ring-0 cursor-pointer`}
           />
           <span className="ml-2 text-gray-700 capitalize">{lbl}</span>
