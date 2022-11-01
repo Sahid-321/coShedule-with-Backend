da2feb22b05373eebfa38263a97e6d0e719d3022
Md Talib Ansari <95065465+talib789@users.noreply.github.com>
2022-11-01 14:22:59 +0530

    completed

--- CoSchedule-merge-f/src/componentsCalendar/Month.js
+++ CoSchedule-merge-f/src/componentsCalendar/Month.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Day from "./Day";
 export default function Month({ month }) {
-  return (
+   return (
     <div className="flex-1 grid grid-cols-7 grid-rows-5">
       {month.map((row, i) => (
         <React.Fragment key={i}>
